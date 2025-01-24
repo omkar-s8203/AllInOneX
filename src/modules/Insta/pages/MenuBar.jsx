57af1e00e28d798ec0b6f812bcf084969c0b59db
omkar suryawanshi <omkar876742@gmail.com>
2025-01-24 23:12:21 +0530

    Add CreatePostForm component and enhance MenuBar for view management

--- src/modules/Insta/pages/MenuBar.jsx
+++ src/modules/Insta/pages/MenuBar.jsx
@@ -1,57 +1,59 @@
-import {  FaMessage, FaPersonRifle, FaSquareMinus, FaThreads } from "react-icons/fa6";
+import { FaMessage, FaPersonRifle, FaSquareMinus, FaThreads } from "react-icons/fa6";
 import "./MenuBar.css";
 import { FaHome, FaInstagram, FaPlusCircle, FaStudiovinari, FaVideo } from "react-icons/fa";
 
-function MenuBar() {
-    return (
-        <div className="menu-bar">
-            <div>
-                <span className="Instagram">
-                    <FaInstagram className="logoi" /> Instagram
-                </span>
-            </div>
+function MenuBar({ setView }) {
+  return (
+    <div className="menu-bar">
+      <div>
+        <span className="Instagram">
+          <FaInstagram className="logoi" /> Instagram
+        </span>
+      </div>
 
-            <div className="menu">
-                <span>
-                    <FaHome /> Posts 
-                </span>
-                <br />
-                <span>
-                    <FaVideo /> Album
-                </span>
-                <br />
-                <span className="message-hover"> 
-                    <FaMessage /> Users
-                </span>
-                {/* <br /> */}
-                {/* <span className="notification-hover">
-                    <FaBell /> ToDo 
-                </span> */}
-                <br />
-                <span className="create-hover">
-                    <FaPlusCircle /> Create
-                </span>
-                <br />
-                <span className="profile-hover">
-                    <FaPersonRifle /> Profile
-                </span>
-                <br />
-            </div>
+      <div className="menu">
+        <span onClick={() => setView("mainContent")} className="menu-item">
+          <FaHome /> Posts
+        </span>
+        <br />
+        <span className="menu-item">
+          <FaVideo /> Album
+        </span>
+        <br />
+        <span className="menu-item">
+          <FaMessage /> Users
+        </span>
+        <br />
+        <span onClick={() => setView("createPost")}  className="menu-item">
+          <FaPlusCircle /> Create
+        </span>
+        <br />
+        <span className="menu-item">
+          <FaPersonRifle /> Profile
+        </span>
+        <br />
+      </div>
 
-            <div className="menu-down">
-                <br />
-                <br />
-                <br />
-                <br />
-                <span><FaStudiovinari/> AI Studio</span>
-                <br />
-                <span><FaThreads/> Threads</span>
-                <br />
-                <span className="setting-hover"><FaSquareMinus/> Setting</span>
-                <br />
-            </div>
-        </div>
-    );
+      <div className="menu-down">
+        <br />
+        <br />
+        <br />
+        <br />
+        <span className="menu-item">
+          <FaStudiovinari /> AI Studio
+        </span>
+        <br />
+        <span className="menu-item">
+          <FaThreads /> Threads
+        </span>
+        <br />
+        <span className="menu-item">
+          <FaSquareMinus /> Setting
+        </span>
+        <br />
+      </div>
+    </div>
+  );
 }
 
 export default MenuBar;
